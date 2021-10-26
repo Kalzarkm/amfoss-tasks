5aefc76d7ba41fa3b6616e8c8663c3d56e71dc9d
Kalzarkm <91386900+Kalzarkm@users.noreply.github.com>
2021-10-26 22:15:35 +0530

    Update Mars.py

--- task-4/Mars.py
+++ task-4/Mars.py
@@ -2,13 +2,13 @@ import requests,json
 import re
 
 data=input("Enter Key: ")
-ida=int(input("Enter id: "))
-base='https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key='
-url=base+data
-#print(url)
+ida=int(input("Enter id(0,1,2..): "))
+idt1=(input("Enter date in form YYYY-MM-DD: "))
+idt2=idt1+"&api_key="
+base='https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='
+url=base+idt2+data
 resp=json.loads(requests.get(url).text)
 rep=dict(resp)
-#print("Dic:\n",resp)
 print("IMG_URL:\n",rep['photos'][ida]['img_src'])
 
 
