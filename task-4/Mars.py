ecea2feecf67bf055635c9a4a62481b9fb78ea8b
Kalzarkm <91386900+Kalzarkm@users.noreply.github.com>
2021-11-19 21:01:03 +0530

    Add files via upload

--- task-4/Mars.py
+++ task-4/Mars.py
@@ -2,13 +2,31 @@ import requests,json
 import re
 
 data=input("Enter Key: ")
-ida=int(input("Enter id(0,1,2..): "))
+roverid=int(input("Enter rover id (5=Curiosity, 6=Opportunity, 7=Spirit): "))
+
+rovna=""
+
+if roverid==5:
+    rovna="curiosity"
+elif roverid==6:
+    rovna="opportunity"
+elif roverid==7:
+    rovna="spirit"
+else:
+    print("Wrong id")
+    exit()
+
 idt1=(input("Enter date in form YYYY-MM-DD: "))
-idt2=idt1+"&api_key="
-base='https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date='
+ida=int(input("Enter image id (0,1,2..): "))
+idt2=rovna+"/photos?earth_date="+idt1+"&api_key="
+
+base='https://api.nasa.gov/mars-photos/api/v1/rovers/' 
+
 url=base+idt2+data
+
 resp=json.loads(requests.get(url).text)
 rep=dict(resp)
+
 print("IMG_URL:\n",rep['photos'][ida]['img_src'])
 
 
